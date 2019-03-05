da90f085162ad4113451418701b966f3bd0bb7a4
Yatin Ajmani <yatinajmani@github.com>
2019-03-06 00:05:28 +0530

    Gradle Assignment added.

---
+++ Mar05_Gradle/src/main/java/com/ttn/Exercise1.java
@@ -0,0 +1,11 @@
+package com.ttn;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(Exercise2.getToken());
+        System.out.println("Reverse from StringUtils : " + StringUtils.reverse("yatin"));
+    }
+}
---
+++ Mar05_Gradle/src/main/java2/com/ttn/Exercise2.java
@@ -0,0 +1,9 @@
+package com.ttn;
+
+import java.util.UUID;
+
+public class Exercise2 {
+    static String getToken(){
+        return UUID.randomUUID().toString();
+    }
+}
